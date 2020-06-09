9f12361daae8b737b2ab49bfe2d81c22ab5836f5
Anup1998 <52277418+Anup1998@users.noreply.github.com>
2020-06-09 13:39:43 +0530

    Update demo.js

--- report-hyperlink/report-link-new-page/demo.js
+++ report-hyperlink/report-link-new-page/demo.js
@@ -34,8 +34,7 @@ visualize({
 
     function collectReportProperties(reportProperties) {
         var newProperties = findReportProperties(location.href);
-        reportProperties =  _.extend(reportProperties, newProperties);
-        console.log('Report Properties:', reportProperties);
+        reportProperties =  _.extend(reportProperties);
         return reportProperties;
     }
     
