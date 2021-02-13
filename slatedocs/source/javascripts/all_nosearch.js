f5a7bde572e54c9660f6fcde9629a3f9386bc305
Sergey Prilukin <spriluki@tibco.com>
2021-02-13 14:50:44 +0200

    Added modification notice to all modified files of the slatedocs project

--- slatedocs/source/javascripts/all_nosearch.js
+++ slatedocs/source/javascripts/all_nosearch.js
@@ -1,3 +1,7 @@
+// This file is part of the slatedocs/slate project.
+// Modified by TIBCO Jaspersoft:
+// - Feb 13 2021: added script for Google analytics, added custom `_fixlinks` script
+
 //= require ./lib/_energize
 //= require ./app/_copy
 //= require ./app/_toc
